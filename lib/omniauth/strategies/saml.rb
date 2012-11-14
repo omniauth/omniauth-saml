022cdadcecc19e62812bf4ca57d94e2f0ad6a156
Xavier Lange <xrlange@gmail.com>
2012-11-14 14:03:23 -0800

    Support different spellings for first and last name

--- lib/omniauth/strategies/saml.rb
+++ lib/omniauth/strategies/saml.rb
@@ -45,8 +45,8 @@ module OmniAuth
         {
           :name  => @attributes[:name],
           :email => @attributes[:email] || @attributes[:mail],
-          :first_name => @attributes[:first_name] || @attributes[:firstname],
-          :last_name => @attributes[:last_name] || @attributes[:lastname]
+          :first_name => @attributes[:first_name] || @attributes[:firstname] || @attributes[:firstName],
+          :last_name => @attributes[:last_name] || @attributes[:lastname] || @attributes[:lastName]
         }
       end
 
