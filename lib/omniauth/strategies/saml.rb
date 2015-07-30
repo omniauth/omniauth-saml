c32d3bc85ae5109c338a20f03e9eed1ceaf8aeb7
Otto Vehvilainen <vehvis@flowdock.com>
2015-07-30 17:10:51 +0300

    Change default to nil

--- lib/omniauth/strategies/saml.rb
+++ lib/omniauth/strategies/saml.rb
@@ -8,7 +8,7 @@ module OmniAuth
 
       option :name_identifier_format, nil
       option :idp_sso_target_url_runtime_params, {}
-      option :compress_request, true
+      option :compress_request, nil
 
       def request_phase
         options[:assertion_consumer_service_url] ||= callback_url
