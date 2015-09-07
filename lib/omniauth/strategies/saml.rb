89c6e20239aae0d96f07dc224a028c62f54d851a
Guillaume Montard <guillaume.montard@gmail.com>
2015-09-07 16:26:04 +0200

    Adding "settings" to Response Class at initialization to handle signing verification

--- lib/omniauth/strategies/saml.rb
+++ lib/omniauth/strategies/saml.rb
@@ -52,8 +52,8 @@ module OmniAuth
           options.idp_cert_fingerprint = fingerprint_exists
         end
 
-        response = OneLogin::RubySaml::Response.new(request.params['SAMLResponse'], options)
-        response.settings = OneLogin::RubySaml::Settings.new(options)
+        settings = OneLogin::RubySaml::Settings.new(options)
+        response = OneLogin::RubySaml::Response.new(request.params['SAMLResponse'], settings: settings)
         response.attributes['fingerprint'] = options.idp_cert_fingerprint
 
         # will raise an error since we are not in soft mode
